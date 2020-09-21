30ce4602fd3ccc4ef267c1f44d840e8c9395f216
shivaniSomvanshi <aashivani2020@gmail.com>
2020-09-21 23:10:40 +0530

    Updates

---
+++ precache-manifest.d2b9c2c552d4e4340263794cdd704907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfae5d453c18854c80986e0824888843",
+    "url": "/Comment-Section/index.html"
+  },
+  {
+    "revision": "566fe65782b0c8699f29",
+    "url": "/Comment-Section/static/js/2.549d875f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Comment-Section/static/js/2.549d875f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bea13382fa593bb64a9",
+    "url": "/Comment-Section/static/js/main.f9769562.chunk.js"
+  },
+  {
+    "revision": "c1c326dce25d319e8441",
+    "url": "/Comment-Section/static/js/runtime-main.06c90f6f.js"
+  }
+]);
